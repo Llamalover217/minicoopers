92880b4cc1e0707e184a621be259f2950bf7c96b
Llamalover217 <17288@burnside.school.nz>
2020-04-24 16:06:05 +1200

    put in the navbar at the top

--- minicooper.py
+++ minicooper.py
@@ -4,8 +4,17 @@ import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home', methods=['GET', 'POST'])
 def home():    # this gets information from the home page file and brings it up
     return render_template('home.html')    # it tells the computer what the file is called that it needs to bring up
+
+@app.route('/mini', methods=['GET', 'POST'])
+def mini():
+    return render_template('minis.html')
+
+@app.route('/bmw', methods=['GET', 'POST'])
+def bmw():
+    return render_template('BMW.html')
 
 if __name__ == '__main__':    # this runs the application
     app.run(debug=True)
