3e714f5988404a4529bf09fc7d42a930deca0156
Llamalover217 <17288@burnside.school.nz>
2020-05-29 11:27:24 +1200

    choose a car and shows info

--- minicooper.py
+++ minicooper.py
@@ -1,7 +1,40 @@
 from flask import Flask, render_template, g, request, redirect   # This imports flask and the other things that this program needs
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+# import sqlite3
 
+# DATABASE = 'database.db'
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
+
+db = SQLAlchemy(app)
+
+class Customer(db.Model):
+    __tablename__ = 'Customer'
+
+    Customer_ID = db.Column(db.Integer, primary_key=True)
+    FirstName = db.Column(db.Text)
+    LastName = db.Column(db.Text)
+
+class Mini(db.Model):
+    __tablename__ = 'Mini'
+
+    Car_ID = db.Column(db.Integer, primary_key=True)
+    CarType = db.Column(db.Text)
+    Power = db.Column(db.Integer)
+    Emissions = db.Column(db.Text)
+    Fuel = db.Column(db.Integer)
+    Speed = db.Column(db.Integer)
+
+class Order(db.Model):
+    __tablename__ = 'Order'
+
+    Order_number = db.Column(db.Integer, primary_key=True)
+    Customer_ID = db.Column(db.ForeignKey('Customer.Customer_ID'))
+    Customer_name = db.Column(db.Text)
+    Car_ID = db.Column(db.ForeignKey('Mini.Car_ID'))
+
+    Mini = db.relationship('Mini', primaryjoin='Order.Car_ID == Mini.Car_ID', backref='orders')
+    Customer = db.relationship('Customer', primaryjoin='Order.Customer_ID == Customer.Customer_ID', backref='orders')
 
 @app.route('/')
 @app.route('/home', methods=['GET', 'POST'])
@@ -9,8 +42,13 @@ def home():    # this gets information from the home page file and brings it up
     return render_template('home.html')    # it tells the computer what the file is called that it needs to bring up
 
 @app.route('/mini', methods=['GET', 'POST'])
-def mini():
-    return render_template('minis.html')
+@app.route('/mini/<int:id>')
+def mini(id=None):
+    minis = Mini.query.all()
+    if id:
+        mini = Mini.query.get_or_404(id)
+        return render_template('mini_chosen.html', minis=minis, mini=mini)
+    return render_template('minis.html', minis=minis)
 
 @app.route('/bmw', methods=['GET', 'POST'])
 def bmw():
