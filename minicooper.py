2f8a89c89a792f7ecf7520bb7e24c60a31ff918b
Llamalover217 <17288@burnside.school.nz>
2020-06-16 14:28:47 +1200

    fixed margins

--- minicooper.py
+++ minicooper.py
@@ -9,6 +9,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 class Customer(db.Model):
+    """This is the Customor Model it represents the the Customor table in the database :)"""
     __tablename__ = 'Customer'
 
     Customer_ID = db.Column(db.Integer, primary_key=True)
@@ -39,25 +40,25 @@ class Order(db.Model):
 
 @app.route('/')
 @app.route('/home', methods=['GET', 'POST'])
-def home():    # this gets information from the home page file and brings it up
+def home():    # defines the home page
     return render_template('home.html')    # it tells the computer what the file is called that it needs to bring up
 
 @app.route('/mini', methods=['GET', 'POST'])
 @app.route('/mini/<int:id>')
-def mini(id=None):
+def mini(id=None):      # This defines the mini page
     minis = Mini.query.all()
-    if id:
+    if id:      # this is how it gets what car the user wants to look at
         mini = Mini.query.get_or_404(id)
         return render_template('mini_chosen.html', minis=minis, mini=mini)
-    return render_template('minis.html', minis=minis)
+    return render_template('minis.html', minis=minis)   # Says where to find the mini page
 
 @app.route('/mini_api/<int:id>')
-def mini_api(id):
+def mini_api(id):       # defines the mini car choosen
     mini = Mini.query.get_or_404(id)
     return render_template('mini_chosen.html', mini=mini)
 
 @app.route('/bmw', methods=['GET', 'POST'])
-def bmw():
+def bmw():      # defines the bmw page
     return render_template('BMW.html')
 
 if __name__ == '__main__':    # this runs the application
