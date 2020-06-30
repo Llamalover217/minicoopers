513d311d2ca6413a0ef1267b2e8173cdd3fc7e72
Llamalover217 <17288@burnside.school.nz>
2020-06-30 14:10:48 +1200

    idk what i did

--- minicooper.py
+++ minicooper.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, g, request, redirect   # This imports flask and the other things that this program needs
+# This imports flask and the other things that this program needs
+from flask import Flask, render_template, g, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 # import sqlite3
 
@@ -8,7 +9,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
 
-class Customer(db.Model):   # This is the Customer Model it represents the Customer table in the database :)
+
+# This is the Customer Model it represents the Customer table in the database :)
+class Customer(db.Model):
     __tablename__ = 'Customer'
 
     Customer_ID = db.Column(db.Integer, primary_key=True)
@@ -26,6 +29,7 @@ class Mini(db.Model):   # This is the Mini model it represents the Mini table in
     Speed = db.Column(db.Integer)
     Image = db.Column(db.Text)
 
+
 class Order(db.Model):   # This is the Order model it represents the Order table in the database
     __tablename__ = 'Order'
 
@@ -34,31 +38,54 @@ class Order(db.Model):   # This is the Order model it represents the Order table
     Customer_name = db.Column(db.Text)
     Car_ID = db.Column(db.ForeignKey('Mini.Car_ID'))
 
-    Mini = db.relationship('Mini', primaryjoin='Order.Car_ID == Mini.Car_ID', backref='orders')
-    Customer = db.relationship('Customer', primaryjoin='Order.Customer_ID == Customer.Customer_ID', backref='orders')
+    Mini = db.relationship(
+        'Mini', primaryjoin='Order.Car_ID == Mini.Car_ID', backref='orders')
+    Customer = db.relationship(
+        'Customer', primaryjoin='Order.Customer_ID == Customer.Customer_ID', backref='orders')
+
+
+class Bmw(db.Model):
+    __tablename__ = 'Bmw'
+
+    carID = db.Column(db.Integer, primary_key=True)
+    carType = db.Column(db.Text)
+    fuel = db.Column(db.Text)
+    emissions = db.Column(db.Text)
+    power = db.Column(db.Text)
+    electricRange = db.Column(db.Text)
+
 
 @app.route('/')
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/home')
 def home():    # defines the home page
-    return render_template('home.html')    # it tells the computer what the file is called that it needs to bring up
+    # it tells the computer what the file is called that it needs to bring up
+    return render_template('home.html')
 
-@app.route('/mini', methods=['GET', 'POST'])
-@app.route('/mini/<int:id>')
+
+@app.route('/mini')
 def mini(id=None):      # This defines the mini page
     minis = Mini.query.all()
-    if id:      # this is how it gets what car the user wants to look at
-        mini = Mini.query.get_or_404(id)
-        return render_template('mini_chosen.html', minis=minis, mini=mini)
-    return render_template('minis.html', minis=minis)   # Says where to find the mini page
+    # Says where to find the mini page
+    return render_template('minis.html', minis=minis)
+
 
 @app.route('/mini_api/<int:id>')
 def mini_api(id):       # defines the mini car choosen
     mini = Mini.query.get_or_404(id)
     return render_template('mini_chosen.html', mini=mini)
 
+
+@app.route('/bmw_api/<int:id>')
+def bmw_api(id):       # defines the mini car choosen
+    bmw = Bmw.query.get_or_404(id)
+    return render_template('bmw_chosen.html', bmw=bmw)
+
+
 @app.route('/bmw', methods=['GET', 'POST'])
 def bmw():      # defines the bmw page
-    return render_template('BMW.html')
+    bmws = Bmw.query.all()
+    return render_template('BMW.html', bmws=bmws)
+
 
 if __name__ == '__main__':    # this runs the application
     app.run(debug=True)
